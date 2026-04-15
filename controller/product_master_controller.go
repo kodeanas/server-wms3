ce3149a81bb67f016c2fafc5a46332a892ddbebf
kodeanas <kodeanas01@gmail.com>
2026-04-15 13:21:27 +0700

    kasih api detail produk dan edit produk

--- controller/product_master_controller.go
+++ controller/product_master_controller.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"errors"
 	"net/http"
 	"wms/services"
 	"wms/utils"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type ProductMasterController struct {
@@ -33,4 +35,50 @@ func (ctl *ProductMasterController) ListStagingSticker(c *gin.Context) {
 		return
 	}
 	utils.SendSuccess(c, masters, "List product master staging_sticker", nil, http.StatusOK)
+}
+
+func (ctl *ProductMasterController) GetDetail(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		utils.SendError(c, http.StatusBadRequest, "id is required")
+		return
+	}
+
+	master, err := ctl.service.GetDetailByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			utils.SendError(c, http.StatusNotFound, "Product master not found")
+			return
+		}
+		utils.SendError(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SendSuccess(c, master, "Detail product master", nil, http.StatusOK)
+}
+
+func (ctl *ProductMasterController) UpdateStaging(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		utils.SendError(c, http.StatusBadRequest, "id is required")
+		return
+	}
+
+	var payload services.UpdateProductMasterStagingPayload
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		utils.SendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	updated, err := ctl.service.UpdateStaging(id, payload)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			utils.SendError(c, http.StatusNotFound, "Product master not found")
+			return
+		}
+		utils.SendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	utils.SendSuccess(c, updated, "Product master staging updated", nil, http.StatusOK)
 }
--- dto/response/product_master.go
+++ dto/response/product_master.go
@@ -44,3 +44,48 @@ type ProductMasterStickerResponse struct {
 	StickerName      *string   `json:"sticker_name"`
 	StickerCodeHex   *string   `json:"sticker_code_hex"`
 }
+
+type ProductMasterDetailResponse struct {
+	ID               string                        `json:"id"`
+	DocumentID       string                        `json:"document_id"`
+	DocumentCode     string                        `json:"document_code"`
+	DocumentName     string                        `json:"document_name"`
+	Barcode          string                        `json:"barcode"`
+	BarcodeWarehouse string                        `json:"barcode_warehouse"`
+	Name             string                        `json:"name"`
+	NameWarehouse    string                        `json:"name_warehouse"`
+	Item             int                           `json:"item"`
+	ItemWarehouse    int                           `json:"item_warehouse"`
+	Price            float64                       `json:"price"`
+	PriceWarehouse   float64                       `json:"price_warehouse"`
+	CategoryID       *string                       `json:"category_id"`
+	StickerID        *string                       `json:"sticker_id"`
+	TypeOut          string                        `json:"type_out"`
+	Location         string                        `json:"location"`
+	CreatedAt        time.Time                     `json:"created_at"`
+	Additional       ProductMasterDetailAdditional `json:"additional"`
+}
+
+type ProductMasterDetailAdditional struct {
+	Document *ProductMasterDocumentAdditional `json:"document"`
+	Sticker  *ProductMasterStickerAdditional  `json:"sticker"`
+	Category *ProductMasterCategoryAdditional `json:"category"`
+}
+
+type ProductMasterDocumentAdditional struct {
+	Code     string `json:"code"`
+	NameFile string `json:"name_file"`
+}
+
+type ProductMasterStickerAdditional struct {
+	Name    string `json:"name"`
+	CodeHex string `json:"code_hex"`
+	Type    string `json:"type"`
+}
+
+type ProductMasterCategoryAdditional struct {
+	Name    string `json:"name"`
+	Diskon  int    `json:"diskon"`
+	Type    string `json:"type"`
+	CodeHex string `json:"code_hex"`
+}
