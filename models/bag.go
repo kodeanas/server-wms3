91c6fa64599a2e3388caa63cdb3a339ea65492d8
Fajar <fajar.putra1691@gmail.com>
2026-04-16 11:56:52 +0700

    Memperbaiki error pada relasi bags_user_id

--- models/bag.go
+++ models/bag.go
@@ -8,15 +8,15 @@ import (
 )
 
 type Bag struct {
-	ID               uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	Code             string         `gorm:"size:255;not null" json:"code"`
-	Type             string         `gorm:"size:50" json:"type"`
-	UserID           uuid.UUID      `gorm:"type:uuid" json:"user_id"`
-	IsMoved          bool           `gorm:"default:false" json:"is_moved"`
-	DateOut          *time.Time     `json:"date_out"`
-	CargoID          *uuid.UUID     `gorm:"type:uuid" json:"cargo_id"`
-	TransferStoreID  *uuid.UUID     `gorm:"type:uuid" json:"transfer_store_id"`
-	CreatedAt        time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt        time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
-	DeletedAt        gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	ID              uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	Code            string         `gorm:"size:255;not null" json:"code"`
+	Type            string         `gorm:"size:50" json:"type"`
+	UserID          *uuid.UUID     `gorm:"type:uuid" json:"user_id"`
+	IsMoved         bool           `gorm:"default:false" json:"is_moved"`
+	DateOut         *time.Time     `json:"date_out"`
+	CargoID         *uuid.UUID     `gorm:"type:uuid" json:"cargo_id"`
+	TransferStoreID *uuid.UUID     `gorm:"type:uuid" json:"transfer_store_id"`
+	CreatedAt       time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt       time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt       gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
